5003de56ddbf848c715ceda049c67c90bbcf1916
Raúl Gómez <raul_nouni@hotmail.com>
2018-09-04 21:08:40 -0500

    Instalando React y ReactDOM

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log('Hola Mundo')
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const app = document.getElementById('app')
+const holaMundo = <h1>Hola Mundo Como estas</h1>
+ReactDOM.render(holaMundo ,app)
