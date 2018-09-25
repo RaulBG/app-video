83dd19b68e0a6dfd5e7dd99b33bb902e63560404
Raúl Gómez <raul_nouni@hotmail.com>
2018-09-24 22:44:26 -0500

    19. Estructura para tener components, UI y mostrar categories

--- src/entries/home.js
+++ src/entries/home.js
@@ -2,9 +2,9 @@ import React from 'react'
 import { render } from 'react-dom'
 // import Playlist from './src/playlist/components/playlist'
 import Home from '../pages/containers/home'
-// import data from './src/api.json'
+import data from '../api.json'
 
 
 const app = document.getElementById('app')
 // const holaMundo = <h1>Hola Mundo Como estas</h1>
-render(<Home />, app)
+render(<Home data={data} />, app)
