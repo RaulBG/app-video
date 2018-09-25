d547f010c829c07b5b3a988c1b8bd58e5ab3c53b
Raúl Gómez <raul_nouni@hotmail.com>
2018-09-24 22:14:12 -0500

    18. Reestructurando con containers y UI components

--- index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { render } from 'react-dom'
-import Playlist from './src/playlist/components/playlist'
-import data from './src/api.json'
-
-
-const app = document.getElementById('app')
-// const holaMundo = <h1>Hola Mundo Como estas</h1>
-render(<Playlist data={data} />,app)
---
+++ src/entries/home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { render } from 'react-dom'
+// import Playlist from './src/playlist/components/playlist'
+import Home from '../pages/containers/home'
+// import data from './src/api.json'
+
+
+const app = document.getElementById('app')
+// const holaMundo = <h1>Hola Mundo Como estas</h1>
+render(<Home />, app)
