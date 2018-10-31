dbc4626b88bb17e4e3f81f3db241ec1ef830b9fb
Raul Gomez <raul_nouni@hotmail.com>
2018-10-30 22:56:24 -0600

    24. UI de Formularios

---
+++ src/widgets/components/search.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './search.css'
+
+// function Search (props) {
+//     return (
+//         <form></form>
+//     )
+// }
+
+const Search = () => (
+    <form className="Search">
+        <input type="text" placeholder="Buscar video.." className="Search-input" />
+    </form>
+)
+
+export default Search
