710247a54c3c8a3b46380f9f499b30d828d79d91
Raul Gomez <raul_nouni@hotmail.com>
2018-11-01 20:47:41 -0600

    27. Componentes titulo y fuente de video

--- src/player/containers/video-player.js
+++ src/player/containers/video-player.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react'
 import VideoPlayerLayout from '../components/video-player-layout'
+import Video from '../components/video'
+import Title from '../components/title'
 
 class VideoPlayer extends Component {
   render () {
     return (
       <VideoPlayerLayout>
-        <video autoPlay controls src="http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4" />
+        <Title 
+          title="Video pro"
+        />
+        <Video 
+          autoplay={false}
+          src="http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4"
+          />
       </VideoPlayerLayout>
     )
   }
