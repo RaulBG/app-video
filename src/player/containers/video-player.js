fa32b45e35846d703c2adc264b07d80ccb9eeff2
Raul Gomez <raul_nouni@hotmail.com>
2018-11-06 23:02:58 -0600

    28. UI Play Pause

--- src/player/containers/video-player.js
+++ src/player/containers/video-player.js
@@ -2,14 +2,24 @@ import React, { Component } from 'react'
 import VideoPlayerLayout from '../components/video-player-layout'
 import Video from '../components/video'
 import Title from '../components/title'
+import PlayPause from '../components/play-pause'
 
 class VideoPlayer extends Component {
+  state = {
+    pause: true
+  }
+  togglePlay = (evt) => {
+    this.setState({
+      pause: !this.state.pause
+    })
+  }
   render () {
     return (
       <VideoPlayerLayout>
         <Title 
           title="Video pro"
         />
+        <PlayPause pause={this.state.pause} handleClick={this.togglePlay} />
         <Video 
           autoplay={false}
           src="http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4"
